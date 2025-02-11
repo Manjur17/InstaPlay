02e4155354a334e0502fb3ed263c4818e67891c2
Manjur17 <skmanjuralli9@gmail.com>
2025-02-11 12:24:06 +0530

    "final css changes"

--- src/components/MovieTitle/MovieTitle.jsx
+++ src/components/MovieTitle/MovieTitle.jsx
@@ -2,15 +2,29 @@ import React, { useState, useRef, useEffect } from "react";
 import "./MovieTitle.css";
 
 const MovieTitle = ({ title }) => {
-    
     const [isTruncated, setIsTruncated] = useState(false);
     const titleRef = useRef(null);
 
     useEffect(() => {
+
+        checkOverflow(); // Initial check
+
+        // Use ResizeObserver to handle dynamic changes in width
+        const resizeObserver = new ResizeObserver(checkOverflow);
+        if (titleRef.current) {
+            resizeObserver.observe(titleRef.current);
+        }
+
+        return () => {
+            resizeObserver.disconnect();
+        };
+    }, [title]);
+
+    const checkOverflow = () => {
         if (titleRef.current) {
             setIsTruncated(titleRef.current.scrollWidth > titleRef.current.clientWidth);
         }
-    }, [title]);
+    };
 
     return (
         <div className="tooltip">
